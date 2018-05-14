8ba67fb837364929c18d4c939a2e9f47163be221
Lmsgcc <34246225+Lmsgcc@users.noreply.github.com>
2018-05-14 21:10:37 +0100

    asdfasdfasdf

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -7,9 +7,33 @@ import { AuthService } from '../shared/auth.service';
   styleUrls: ['./settings.component.css']
 })
 export class SettingsComponent implements OnInit{
-  constructor(private auth: AuthService) {  }
+  constructor(
+    private auth: AuthService,
+    private af: AngularFirestore
+  ) {  }
+
+  public settings: {   value1: string, value2: string, value3: string, value4: string} 
+  = {  value1: "", value2: "", value3: "", value4: ""};
+
+  public id : string;
+
   ngOnInit(): void { 
     this.auth.userState
       .subscribe(x => this.auth.isLoggedIn = x);
+  }
+
+  public saveData():void
+  {
+    if (this.id != "new") {
+      this.af.doc("/settings/" + this.id)
+      .update(this.settings)
+      .then(x => {
+      })
+    } else {
+      this.af.collection("/settings")
+      .add(this.settings)
+      .then(x => {
+      })
+    }
   }
 }
